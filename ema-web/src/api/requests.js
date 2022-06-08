33ed252228db29babcb7ea9079b3210ab33db573
Omar Reda <omarredaelsayedmohamed@gmail.com>
2022-06-08 15:55:15 -0700

    token test

--- ema-web/src/api/requests.js
+++ ema-web/src/api/requests.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 
 const BASE_URL = 'http://localhost:5000/api'
 
-const TOKEN = JSON.parse(localStorage.getItem('user'))
 
 export const authRequest = axios.create({
     baseURL: BASE_URL
@@ -10,5 +9,5 @@ export const authRequest = axios.create({
 
 export const userRequest = axios.create({
     baseURL: BASE_URL,
-    headers: { token: TOKEN.token }
+    headers: { token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJuYW1lIjoi2YXYrdmF2K8g2LXZhNin2K0iLCJlbWFpbCI6Im1vc2FsYWhAZ21haWwuY29tIiwicGFzc3dvcmQiOiJtb3NhbGFoNzciLCJwaG9uZSI6IjAxMDY1NjMwMzMyIiwicm9sZSI6ItmF2KfZhNmDIiwicmVnaXN0cmF0aW9uZGF0ZSI6IjIwMjItMDYtMDhUMDA6MDA6MDAuMDAwWiJ9LCJpYXQiOjE2NTQ2ODYzMDYsImV4cCI6MTY1NzI3ODMwNn0.jyO1RS4en3PfgAjZvbWBTQJPNrCRhPY3pbydW9CzkgA' }
 })
