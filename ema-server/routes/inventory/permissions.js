4f5611fb14608124ab4134e8d381812a91285934
Omar Reda <omarredaelsayedmohamed@gmail.com>
2022-08-14 10:29:08 -0700

    removed all tokens

--- ema-server/routes/inventory/permissions.js
+++ ema-server/routes/inventory/permissions.js
@@ -4,40 +4,40 @@ const { verify } = require('jsonwebtoken')
 const permissionController = require('../../controllers/inventory/permissions')
 const { adminPermission, adminAndEmployeePermission } = require('../../middlewares/verifyToken')
 
-router.post('/receive-permissions', adminAndEmployeePermission, (request, response) => permissionController.addReceivePermission(request, response))
+router.post('/receive-permissions', (request, response) => permissionController.addReceivePermission(request, response))
 
-router.get('/receive-permissions', adminAndEmployeePermission, (request, response) => permissionController.getReceivePermissions(request, response))
+router.get('/receive-permissions', (request, response) => permissionController.getReceivePermissions(request, response))
 
-router.get('/receive-permissions/users/:userId', adminAndEmployeePermission, (request, response) => permissionController.getUserReceivePermission(request, response))
+router.get('/receive-permissions/users/:userId', (request, response) => permissionController.getUserReceivePermission(request, response))
 
-router.get('/exchange-permissions', adminAndEmployeePermission, (request, response) => permissionController.getExchangePermissions(request, response))
+router.get('/exchange-permissions', (request, response) => permissionController.getExchangePermissions(request, response))
 
-router.post('/exchange-permissions', adminAndEmployeePermission, (request, response) => permissionController.addExchangePermission(request, response))
+router.post('/exchange-permissions', (request, response) => permissionController.addExchangePermission(request, response))
 
-router.get('/exchange-permissions/users/:userId', adminAndEmployeePermission, (request, response) => permissionController.getUserExchangePermission(request, response))
+router.get('/exchange-permissions/users/:userId', (request, response) => permissionController.getUserExchangePermission(request, response))
 
-router.get('/receive-permissions/:permissionId', adminAndEmployeePermission, (request, response) => permissionController.getReceivePermission(request, response))
+router.get('/receive-permissions/:permissionId', (request, response) => permissionController.getReceivePermission(request, response))
 
-router.get('/exchange-permissions/:permissionId', adminAndEmployeePermission, (request, response) => permissionController.getExchangePermission(request, response))
+router.get('/exchange-permissions/:permissionId', (request, response) => permissionController.getExchangePermission(request, response))
 
-router.patch('/receive-permissions/:permissionId', adminPermission, (request, response) => permissionController.updateReceivePermissionProvider(request, response))
+router.patch('/receive-permissions/:permissionId', (request, response) => permissionController.updateReceivePermissionProvider(request, response))
 
-router.patch('/exchange-permissions/:permissionId', adminPermission, (request, response) => permissionController.updateExchangePermissionClient(request, response))
+router.patch('/exchange-permissions/:permissionId', (request, response) => permissionController.updateExchangePermissionClient(request, response))
 
 router.get('/receive-permissions/providers/:providerId', (request, response) => permissionController.getProviderPermissions(request, response))
 
 router.get('/exchange-permissions/clients/:clientId', (request, response) => permissionController.getClientPermissions(request, response))
 
-router.delete('/permissions/:permissionId/:permissionType', adminPermission, (request, response) => permissionController.deletePermissiosAndUpdateItems(request, response))
+router.delete('/permissions/:permissionId/:permissionType', (request, response) => permissionController.deletePermissiosAndUpdateItems(request, response))
 
-router.patch('/receive-permissions/receive-permissions-items/:receivePermissionItemId/quantity', adminPermission, (request, response) => permissionController.editeReceivePermissionQuantity(request, response))
+router.patch('/receive-permissions/receive-permissions-items/:receivePermissionItemId/quantity', (request, response) => permissionController.editeReceivePermissionQuantity(request, response))
 
-router.patch('/exchange-permissions/exchange-permissions-items/:exchangePermissionItemId/quantity', adminPermission, (request, response) => permissionController.editeExchangePermissionQuantity(request, response))
+router.patch('/exchange-permissions/exchange-permissions-items/:exchangePermissionItemId/quantity', (request, response) => permissionController.editeExchangePermissionQuantity(request, response))
 
-router.patch('/receive-permissions/receive-permissions-items/:receivePermissionItemId/price', adminPermission, (request, response) => permissionController.editeReceivePermissionPrice(request, response))
+router.patch('/receive-permissions/receive-permissions-items/:receivePermissionItemId/price', (request, response) => permissionController.editeReceivePermissionPrice(request, response))
 
-router.delete('/receive-permissions/receive-permissions-items/:receivePermissionItemId', adminPermission, (request, response) => permissionController.deleteReceivePermissionItem(request, response))
+router.delete('/receive-permissions/receive-permissions-items/:receivePermissionItemId', (request, response) => permissionController.deleteReceivePermissionItem(request, response))
 
-router.delete('/exchange-permissions/exchange-permissions-items/:exchangePermissionItemId', adminPermission, (request, response) => permissionController.deleteExchangePermissionItem(request, response))
+router.delete('/exchange-permissions/exchange-permissions-items/:exchangePermissionItemId', (request, response) => permissionController.deleteExchangePermissionItem(request, response))
 
 module.exports = router
--- ema-server/routes/inventory/users.js
+++ ema-server/routes/inventory/users.js
@@ -4,18 +4,18 @@ const { adminPermission, adminAndEmployeePermission } = require('../../middlewar
 
 router.post('/users', (request, response) => usersController.addUser(request, response))
 
-router.get('/users', adminPermission, (request, response) => usersController.getUsers(request, response))
+router.get('/users', (request, response) => usersController.getUsers(request, response))
 
-router.get('/users/employees', adminPermission, (request, response) => usersController.getEmployees(request, response))
+router.get('/users/employees', (request, response) => usersController.getEmployees(request, response))
 
-router.get('/users/admins', adminPermission, (request, response) => usersController.getAdmins(request, response))
+router.get('/users/admins', (request, response) => usersController.getAdmins(request, response))
 
-router.put('/users/employees/:employeeId', adminPermission, (request, response) => usersController.updateEmployee(request, response))
+router.put('/users/employees/:employeeId', (request, response) => usersController.updateEmployee(request, response))
 
-router.patch('/users/employees/:employeeId/block', adminPermission, (request, response) => usersController.blockEmployee(request, response))
+router.patch('/users/employees/:employeeId/block', (request, response) => usersController.blockEmployee(request, response))
 
-router.patch('/users/employees/:employeeId/enable', adminPermission, (request, response) => usersController.enableEmployee(request, response))
+router.patch('/users/employees/:employeeId/enable', (request, response) => usersController.enableEmployee(request, response))
 
-router.delete('/users/employees/:employeeId', adminPermission, (request, response) => usersController.deleteEmployee(request, response))
+router.delete('/users/employees/:employeeId', (request, response) => usersController.deleteEmployee(request, response))
 
 module.exports = router
